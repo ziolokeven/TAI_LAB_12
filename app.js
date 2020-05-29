5171e21df80c6b537f53366a4f944624c3935882
ziolokeven <ziolokeven@gmail.com>
2020-05-29 14:24:19 +0200

    fix lab12

--- app.js
+++ app.js
@@ -35,9 +35,6 @@ process.on('SIGINT', () => {
 
 
 routes(app);
-app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
 app.listen(config.port, () => {
   console.info(`Server is running at ${config.port}`)
 });
